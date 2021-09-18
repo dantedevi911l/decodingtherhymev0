e923a1923ee2741c18cc0b8779d1359e093bc714
piyush <piyushsuyip911@gmail.com>
2021-09-18 19:12:15 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -2,8 +2,13 @@
 var bg,sheep,sheepImg,wool,woolImg,bags,bagsImg,next,nextImg;
 //declaring and initialising gameStates
 
-var gameState ;
+var gameState = 0; ;
+var preAndPostStrings = {"sheep": [["Baa Baa "], ["Baa Baa Black Sheep "]],
+                         "wool": [["Have you any "], ["Have you any wool"]]
+                      };
 
+var states = { "sheep": 0 ,
+                "wool": 0};
 
 function preload(){
   //loading the images
@@ -24,26 +29,57 @@ function setup() {
   //next button
   next = createButton("NEXT");
   next.position(1400, 800);
-  next.size(100,50)
+  next.size(100,50);
  
   //sheep 
   sheep = createSprite(950,100,200,200)
-  sheep.visible = false;
-  sheep.addImage('s',sheepImg)
-  sheep.scale = 0.15
+  sheep.visible = true;
+  sheep.addImage('s',sheepImg);
+  sheep.scale = 0.15;
 
   //wool
   wool = createSprite(950,300,200,200)
-  wool.visible = false;
-  wool.addImage('w',woolImg)
-  wool.scale = 0.15
+  wool.visible = true;
+  wool.addImage('w',woolImg);
+  wool.scale = 0.15;
+
+
+  background(bg); 
+
+  
+
+  drawSprites();
+
+  //Prinitng The 1st line
+  fill("blue");
+  textStyle(BOLDITALIC);
+  textSize(45);
+  strokeWeight(104);
+  text("Baa Baa  " , 500, 100);
+  
+
+  //Prinitng The 2nd line
+  fill("blue");
+  textStyle(BOLDITALIC);
+  textSize(45);
+  strokeWeight(104);
+  text("Have you any  " , 500, 300);
+  
+
+  
+
 }
 
 function draw() {
+  
+  //background(bg);
+  //drawSprites();
+  //next.mousePressed(textDisplay);
+  
   background(bg);  
   
  
-  next.mousePressed(ImageDisplay)
+  next.mousePressed(textDisplay);
 
   
 
@@ -54,23 +90,33 @@ function draw() {
   textStyle(BOLDITALIC)
   textSize(45);
   strokeWeight(104);
-  text("Baa Baa Black  " , 500, 100);
+  text(preAndPostStrings["sheep"][ states["sheep"] ] , 500, 100);
+  
 
   //Prinitng The 2nd line
   fill("blue");
   textStyle(BOLDITALIC)
   textSize(45);
   strokeWeight(104);
-  text("Have you any  " , 500, 300);
-
-  
+  text(preAndPostStrings["wool"][ states["wool"] ] , 500, 300);
 
 
 
 }
 
-function ImageDisplay(){
-  sheep.visible = true;
+function textDisplay(){
+  if( gameState == 0 ){
+
+    gameState += 1;
+    removeSprite(sheep);
+    states["sheep"]+= 1;
+
+  }else if(gameState == 1){
+    
+    removeSprite(wool);
+    states["wool"]+= 1;
+    gameState+= 1;
+  }
   
 }
 
